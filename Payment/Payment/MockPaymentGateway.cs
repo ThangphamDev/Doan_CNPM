008b3851fb0b7ef2383d75e40be73e51dbd8e310
thang <thangphamxuan097@gmail.com>
2025-04-07 23:56:30 +0700

    Tao class Order và mock service cho Payment Gateway

---
+++ Payment/Payment/MockPaymentGateway.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment
+{
+    internal class MockPaymentGateway
+    {
+        public bool ProcessPayment(decimal amount, string method)
+        {
+            // Giả lập: nếu tổng tiền < 1,000,000 thì thành công, ngược lại thất bại
+            return amount < 1000000;
+        }
+    }
+}
