18122a60c457ae6717308a5d3e48c355a9eed1dc
thang <thangphamxuan097@gmail.com>
2025-04-08 00:05:02 +0700

    Binding du  lieu thành table voi AppDbContext

---
+++ Payment/Payment/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public AppDbContext()
+            : base(new DbContextOptionsBuilder<AppDbContext>()
+                  .UseInMemoryDatabase("PaymentAppDb")
+                  .Options)
+        {
+        }
+    }
+}
